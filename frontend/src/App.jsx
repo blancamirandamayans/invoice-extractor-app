7422ba4decfa0be1e78766e8897ac0a43af978d1
blancamirandamayans <blanca.mayans@gmail.com>
2025-07-28 12:45:53 +0200

    style: improve style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,17 +16,20 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center p-10">
-      <h1 className="text-2xl font-bold mb-4">Invoice Extractor</h1>
-      <input type="file" onChange={e => setFile(e.target.files[0])} />
-      <button
-        onClick={uploadFile}
-        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded"
-      >
-        Upload & Extract
-      </button>
+    <div className='container'>
+      <div >
+        <h1>Invoice Extractor</h1>
+        <input type="file" onChange={e => setFile(e.target.files[0])} />
+        <br></br>
+        <button
+          onClick={uploadFile}
+          className="mt-4 px-4 py-2 bg-blue-600 text-white rounded"
+          >
+          Upload & Extract
+        </button>
+      </div>
       {data && (
-        <div className="mt-6 bg-white shadow p-4 rounded">
+        <div className='result'>
           <p><strong>Supplier:</strong> {data.supplier}</p>
           <p><strong>Date:</strong> {data.date}</p>
           <p><strong>Total Amount:</strong> {data.total_amount} ({data.currency})</p>
